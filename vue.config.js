8b3d1d112df53e81ad33e0224b578cdb804dbd2b
Daniel Rebolledo <danielrebolledo.182@gmail.com>
2022-06-04 16:02:44 -0400

    github pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Desafio_idolos_de_las_artes_marciales/'
+    : '/'
+
 })
